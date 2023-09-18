bc6954555526b48edecaf5c919f324c1cf0d0b9d
Valentyna1402 <131916763+Valentyna1402@users.noreply.github.com>
2023-09-18 14:31:02 +0000

    Deploying to gh-pages from @ Valentyna1402/goit-js-hw-09@b719a68201d98d4b8c5c0a02f86052e0ca581e2c 🚀

--- 01-color-switcher.25d64656.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.25d64656.js.map
